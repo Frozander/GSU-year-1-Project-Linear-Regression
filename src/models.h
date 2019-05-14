129fa84f7a6021348ec2014aaa27b64772b7b9c5
Frozander <Frozander@users.noreply.github.com>
2019-05-14 19:06:39 +0300

    >Added new function create_matrix()
    [create_matrix]
    >Takes line and collumn values and creates a two dimentional array.
    >And sets all values as 0s.

--- src/models.h
+++ src/models.h
@@ -8,7 +8,14 @@
 
 
 // Gerekli veri tanimlarini burada yapabilirsiniz
-
+typedef struct matrix
+{
+    // Lines and collumns for the matrix
+    int lines;
+    int collmuns;
+    // Two dimensional array for values
+    double** values;
+} Matrix;
 
 int model_by_similarity(House* houses,House new_house);
 void create_data_matrices(House* houses,int** X,int* y);
@@ -16,5 +23,7 @@ int** get_transpose(int** A);
 int** get_inverse(int** A);
 int** get_multiplication(int** A, int** B);
 int** calculate_parameter(House* houses);
+
+Matrix* create_matrix(int line, int collumn);
 
 #endif
